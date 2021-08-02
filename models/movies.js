cfd2e58835e9c9dcae952750c0e14f5aa6ae7f09
maria-okoledova394 <maria-okoledova@yandex.ru>
2021-08-02 22:00:13 +0300

    Create schemes and models for users and movies

---
+++ models/movies.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const urlRegex = /(http|https):\/\/(www\.)?([a-zA-Z0-9_-]+)\.([a-zA-Z]+)(\/[a-zA-Z0-9_\-._~:/?#[\]@!$&'()*+,;=]*)?/;
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => urlRegex.test(v),
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => urlRegex.test(v),
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => urlRegex.test(v),
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    ref: 'movie',
+    required: true,
+    unique: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
