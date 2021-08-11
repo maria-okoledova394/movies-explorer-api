7a50917e3afe566f0ae3c5a8dd793bb2b821b2ab
maria-okoledova394 <maria-okoledova@yandex.ru>
2021-08-11 15:22:12 +0300

    Add rate limit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
 const handleErrors = require('./middlewares/handle-errors');
+const limiter = require('./utils/rate-limiter');
 // const cors = require('./middlewares/cors');
 const NotFoundError = require('./errors/not-found-err');
 const { PORT, URL } = require('./utils/config');
@@ -27,9 +28,9 @@ mongoose.connect(URL, {
 // app.use(cors);
 app.use(cookieParser());
 app.use(helmet());
+app.use(limiter);
 app.use(requestLogger);
 app.use(routes);
-
 app.use(errorLogger);
 
 app.use((req, res, next) => {
