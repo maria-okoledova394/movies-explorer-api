83faa0d2012524abd31722bb8fc561775fc85471
maria-okoledova394 <maria-okoledova@yandex.ru>
2021-08-11 03:25:44 +0300

    Use database URL from .env

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ const auth = require('./middlewares/auth');
 const { createUser, login, logout } = require('./controllers/users');
 const NotFoundError = require('./errors/not-found-err');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, URL } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/moviesdb', {
+mongoose.connect(URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
