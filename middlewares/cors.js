4689c0af0aaf9048dbf15735772a473ee9d82ea1
maria-okoledova394 <maria-okoledova@yandex.ru>
2021-09-19 01:19:43 +0300

    Fix typo

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,12 +14,13 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
+    // res.header('Access-Control-Allow-Credentials', true);
     res.status(200).send();
     return;
   }
